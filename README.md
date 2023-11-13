c6213272934c1b19f465094ab9999df1ba04be1a
SleepyLe0 <kunkundids@gmail.com>
2023-11-13 14:27:25 +0700

    Draft#1

---
+++ README.md
@@ -0,0 +1,30 @@
+# ## TypeScript Problem: User Information Logger
+
+### Problem Statement:
+
+Create a TypeScript function that takes a user object as a parameter and logs a personalized message.
+
+The user object has the following structure:
+
+```typescript
+type User = {
+  id: number;
+  fullName: {
+    firstName: string;
+    lastName: string;
+  };
+  age: number;
+  contact: {
+    email: string;
+    phone: {
+      countryCode: string;
+      number: string;
+    };
+  };
+  address: {
+    street: string;
+    city: string;
+    zipCode: string;
+  };
+  hobbies: string[];
+};
--- index.ts
+++ index.ts
@@ -1,4 +1,38 @@
-const sum = (a: number, b: number) => a + b;
+type User = {
+    id: number;
+    fullName: {
+        firstName: string;
+        lastName: string;
+    };
+    age: number;
+    contact: {
+        email: string;
+        phone: {
+            countryCode: string;
+            number: string;
+        };
+    };
+    address: {
+        street: string;
+        city: string;
+        zipCode: string;
+    };
+    hobbies: string[];
+};
 
-console.log(sum(1, 2));
+function logUserInfo( user:User ) {
+    // write your statement here
+}
+
+// test
+logUserInfo({
+    id: 1,
+    fullName: { firstName: "John", lastName: "Doe" },
+    age: 25,
+    contact: { email: "john.doe@example.com", phone: { countryCode: "1", number: "555-1234" } },
+    address: { street: "123 Main St", city: "Anytown", zipCode: "12345" },
+    hobbies: ["reading", "coding", "gaming"]
+});
+
+
 
